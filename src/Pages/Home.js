8173109fcd30071f3b5ff65532e853596da2b5b7
2109231Frank <2109231@students.wits.ac.za>
2022-05-13 09:25:32 +0200

    First Commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import "../Style/Home.css";
-import MainIcon from "../Images/MainIcon.jpg";
+import "../Style/Blogs.css";
+import { Bloglist } from '../Helpers/BlogPosts';
+import BlogItem from "../Components/BlogItem";
 
 export default class Home extends Component {
   render() {
     return (
+    <div className='bigpage'>
       <div className='Home'>
         <div className='HeaderContainer'>
             <h1 className='homeh1'>FRANCO HAYES PHOTOGRAPHY</h1>
@@ -14,6 +17,23 @@ export default class Home extends Component {
             </p>
         </div>
       </div>
+
+      <div className='Blogs'>
+      <h1>INTERACTIVE MEDIA BLOGS</h1>
+          <div className='bloglist'>
+              {Bloglist.map((blogItem, value) => {
+                  return(
+                      <BlogItem
+                      value = {value}
+                      title = {blogItem.title}
+                      intro = {blogItem.intro}
+                      link = {blogItem.link}
+                      />
+                  )
+              })}
+          </div>
+      </div>
+    </div>
     )
   }
 }
